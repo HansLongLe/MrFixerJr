529bcafd42a205f3057e140cc01de598326bf764
Iana01 <304119@via.dk>
2021-04-07 11:32:00 +0200

    View update

--- src/View/LoginController.java
+++ src/View/LoginController.java
@@ -1,4 +1,11 @@
 package View;
 
-public class LoginController {
+public class LoginController
+{
+  private LoginViewModel loginViewModel;
+
+  public void init(ViewModelFactory viewModelFactory, ViewHAndler viewHAndler)
+  {
+
+  }
 }
---
+++ src/View/ViewHandler.java
@@ -0,0 +1,21 @@
+package View;
+
+import javafx.stage.Stage;
+
+public class ViewHandler
+{
+  public ViewHandler(ViewModelFactory ViewModel, Stage stage)
+  {
+
+  }
+
+  public void openCreateAccount()
+  {
+
+  }
+
+  public void openLogIn()
+  {
+
+  }
+}
