c712de967c353562816937ffe28afadefbfe0594
Roalson40 <81298155+Roalson40@users.noreply.github.com>
2021-04-07 14:44:22 +0200

    ii
    jj

--- src/Client/View/CreateAccountController.java
+++ src/Client/View/CreateAccountController.java
@@ -1,13 +1,18 @@
 package Client.View;
 
 import Client.View.ViewHandler;
+<<<<<<< Updated upstream
 import Client.ViewModel.CreateAccountViewModel;
 import Client.ViewModel.ViewModelFactory;
+=======
+import ViewModel.CreateAccountViewModel;
+import ViewModel.ViewModelFactory;
+import javafx.fxml.FXML;
+>>>>>>> Stashed changes
 
 public class CreateAccountController
 {
   private CreateAccountViewModel createAccountViewModel;
-
   public void init(ViewModelFactory viewModelFactory, ViewHandler viewHAndler)
   {
 
