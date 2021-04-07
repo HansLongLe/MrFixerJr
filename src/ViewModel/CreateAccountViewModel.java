822b33481a000e4e433ad369926cfed23c110742
Chen <chen2468830035@gmail.com>
2021-04-07 11:34:47 +0200

    ViewModel

---
+++ src/ViewModel/CreateAccountViewModel.java
@@ -0,0 +1,10 @@
+package ViewModel;
+
+import Model.DataModel;
+
+public class CreateAccountViewModel {
+    private DataModel model;
+    public CreateAccountViewModel(DataModel model){
+        this.model=model;
+    }
+}
