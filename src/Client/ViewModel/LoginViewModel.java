590c097a3e527bed4486ed024cc656280fe16f4f
Iana01 <304119@via.dk>
2021-04-07 11:53:24 +0200

    ViewModel classes

---
+++ src/Client/ViewModel/LoginViewModel.java
@@ -0,0 +1,13 @@
+package ViewModel;
+
+import Model.DataModel;
+
+public class LoginViewModel {
+    private DataModel model;
+
+    public LoginViewModel(DataModel model){
+        this.model=model;
+    }
+
+
+}
