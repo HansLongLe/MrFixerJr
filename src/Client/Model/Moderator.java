ceae574fa82fc1924a8b56f08fbb343dbad77ae6
Iana01 <304119@via.dk>
2021-04-07 11:46:47 +0200

    All packages And the network in the Client

---
+++ src/Client/Model/Moderator.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Moderator extends User
+{
+  public Moderator(String userName, String password)
+  {
+    super(userName, password);
+  }
+
+}
---
+++ src/Client/View/CreateAccountController.java
@@ -0,0 +1,11 @@
+package View;
+
+public class CreateAccountController
+{
+  private CreateAccountViewModel createAccountViewModel;
+
+  public void init(ViewModelFactory viewModelFactory, ViewHAndler viewHAndler)
+  {
+
+  }
+}
