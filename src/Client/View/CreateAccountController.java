3b4e053b20cd12132157aa093b1f1cdfba4c0865
asold2 <74716450+asold2@users.noreply.github.com>
2021-05-14 14:38:03 +0200

    commit for new user interface

--- src/Client/View/CreateAccountController.java
+++ src/Client/View/CreateAccountController.java
@@ -5,9 +5,13 @@ import Client.View.ViewHandler;
 import Client.ViewModel.CreateAccountViewModel;
 import Client.ViewModel.ViewModelFactory;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class CreateAccountController
 {
@@ -17,9 +21,17 @@ public class CreateAccountController
   @FXML private TextField email;
   @FXML private Button create;
   @FXML private Button back;
+  private CreateAccountViewModel cavm;
+  private ViewHandler vh;
   private CreateAccountViewModel createAccountViewModel;
   public void init(ViewModelFactory viewModelFactory, ViewHandler viewHAndler)
   {
+    this.vh =vh;
+    cavm = viewModelFactory.getCreateAccountViewModel();
+    username.textProperty().bind(cavm.);
+    password.textProperty().bind(cavm.);
+    repeatPassword.textProperty().bind(cavm.);
+    email.textProperty().bind(cavm.);
 
   }
   @FXML
--- src/Client/View/LoginController.java
+++ src/Client/View/LoginController.java
@@ -1,21 +1,38 @@
 package Client.View;
 
 import Client.ViewModel.*;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController
 {
   private LoginViewModel loginViewModel;
+  private CreateAccountViewModel cavm;
+  private ViewHandler vh;
+
+  @FXML private TextField username;
+  @FXML private PasswordField password;
+  @FXML private Button logIn;
+  @FXML private Button createAccount;
+
 
   public void init(ViewModelFactory viewModelFactory, ViewHandler viewHandler)
   {
+    this.vh = viewHandler;
+    loginViewModel = viewModelFactory.getLoginViewModel();
+    username.textProperty().bind(loginViewModel.);
+    password.textProperty().bind(loginViewModel.);
+
+
 
   }
-
+  @FXML
   public void LoginButton(){
 
   }
-
+  @FXML
   public void CreateAccountButton(){
 
   }
