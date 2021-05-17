23eb22cc4cb0fc9f45415f4d728629b19ad5d5c0
Chen <chen2468830035@gmail.com>
2021-05-17 11:43:51 +0200

    chen update

--- src/Server/Network/ServerMain.java
+++ src/Server/Network/ServerMain.java
@@ -9,9 +9,9 @@ public class ServerMain
   public static void main(String[] args)
       throws RemoteException, AlreadyBoundException
   {
-//    System.out.println("Type in the password: \n");
-//    Scanner scanner = new Scanner(System.in);
-//    String password = scanner.nextLine();
+    System.out.println("Type in the password: \n");
+    Scanner scanner = new Scanner(System.in);
+    String password = scanner.nextLine();
     ServerInterface server = new ServerRMI();
     server.start();
   }
